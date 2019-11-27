8ae2a77bc5ef1b9dc469773b59e8617fd418af54
danii <danii@LAPTOP-KU19HP0O>
2019-11-27 16:24:46 +0200

    Add "no args" situation handling

--- src/main/java/ee/smit/homework/classfinder/Main.java
+++ src/main/java/ee/smit/homework/classfinder/Main.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("File nor pattern arguments not found");
+        }
+        
         ClassFinder classFinder = new ClassFinder();
         List<String> matches = classFinder.getMatches(args[0], args[1]);
 
-        for(String string : matches) {
+        for (String string : matches) {
             System.out.println(string);
         }
     }
